921865d38c9bf2db35cb3e0c26582337848a40c7
cpluspluscrowe <ccrowe@unomaha.edu>
2018-07-17 03:20:23 +0000

    auto posting new highlights

--- src/main.go
+++ src/main.go
@@ -5,10 +5,21 @@ import (
 	"highlightDb"
 	"net/http"
 	"safari"
+	"time"
 	"twitter"
 )
 
+func doEvery(d time.Duration, f func()) {
+	for _ = range time.Tick(d) {
+		f()
+	}
+}
+
 func main() {
+	doEvery(5000*time.Millisecond, postHighlightsToTwitter)
+}
+
+func postHighlightsToTwitter() {
 	url := "https://www.safaribooksonline.com/u/8aaaf02c-85f9-42f8-8628-3e0c83d24fd6/"
 	resp, err := http.Get(url)
 	if err != nil {
@@ -22,9 +33,9 @@ func main() {
 	highlightDb.InsertHighlights(highlights)
 
 	dbHighlights := highlightDb.GetUnpostedHighlights()
-	fmt.Printf("Number of highlights to post: %d", len(dbHighlights))
+	fmt.Printf("Number of highlights to post: %d\n", len(dbHighlights))
 	for _, highlight := range dbHighlights {
 		twitter.Tweet(highlight.Text)
-		SetHighlightAsPosted(highlight.Text)
+		highlightDb.SetHighlightAsPosted(highlight.Text)
 	}
 }
